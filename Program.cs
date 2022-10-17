4b6df52271ebdb3581366e70388c7d00de3971f6
Kenzodiac <kennethfujimura@gmail.com>
2022-10-17 15:43:07 -0700

    update

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿//Kenneth Fujimura
-//09-12-2022 @ 9:08pm
-//Mini Challenge #1 - Say Hello
+//10-17-2022
+//GitHub Challenge #1 - Say Hello
 //This is a console project that will ask a user their name and report it back to them with a greeting.
-//Peer Review by: Rafael Manzo, LOTS OF CODE! good stuff, game runs well. kudos.!!
+//Peer Review by:
 
 Console.Clear();
 
+//declaring global variables
 bool playAgain = true;
+bool answerCheck = true;
+string playAgainInput = "";
 
 //play again while loop
 while (playAgain == true) {
@@ -18,19 +21,23 @@ while (playAgain == true) {
 
     //play again prompt with input validation
     Console.WriteLine("Would you like to play again? Y/N");
-    bool answerCheck = true;
     while (answerCheck == true) {
-        string playAgainInput = Console.ReadLine();
-        if (playAgainInput == "y" || playAgainInput == "Y") {
+        playAgainInput = Console.ReadLine();
+        playAgainInput = playAgainInput.ToLower();
+        EvaluatePlayAgain();
+    }
+}
+Console.WriteLine("Bye!");
+
+//custom method to evaluate whether the player wants to play again.
+void EvaluatePlayAgain(){
+    if (playAgainInput == "y" || playAgainInput == "yes") {
             answerCheck = false;
-            continue;
-        } else if (playAgainInput == "n" || playAgainInput == "N"){
+        } else if (playAgainInput == "n" || playAgainInput == "no"){
             playAgain = false;
             answerCheck = false;
         } else {
             Console.WriteLine("I do not understand. Please answer with 'Y' for yes, or 'N' for no.");
             answerCheck = true;
         }
-    }
 }
-Console.WriteLine("Bye!");
